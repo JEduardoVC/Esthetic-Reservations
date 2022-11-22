59f9f843ab321f1f8c752c54b34e6f358b5e5571
Luis Gerardo Valencia Camacho <70810750+LuisGerardoValenciaCamacho@users.noreply.github.com>
2022-11-22 14:30:05 -0600

    Login

--- src/main/java/com/esthetic/reservations/api/repository/ServiceRepository.java
+++ src/main/java/com/esthetic/reservations/api/repository/ServiceRepository.java
@@ -5,16 +5,18 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.esthetic.reservations.api.model.Service;
 
 @Repository
 @Transactional
-@Component("ServiceRepository")
+@EnableJpaRepositories
 public interface ServiceRepository extends GenericRepository<Service, Long> {
 
 	@Query(value = "select * from service where id_branch = :id", nativeQuery = true)
 	List<Service> findAllById_branch(int id);
+	
+	List<Service> findAllByIdBranch(Long idBranch);
 }
