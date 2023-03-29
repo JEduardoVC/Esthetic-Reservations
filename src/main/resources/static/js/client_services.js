188ef9a19b40da75bb1fa48e04dfdb4f4bc9126c
Luis Gerardo Valencia Camacho <gevalencia99@gmail.com>
2023-03-29 16:12:57 -0600

    Update de contraseña y actualización de reservaciones

--- src/main/resources/static/js/client_services.js
+++ src/main/resources/static/js/client_services.js
@@ -15,10 +15,14 @@ const branchId = sessionStorage.getItem("branchId");
 const myHeaders = new Headers();
 myHeaders.append("Authorization", `Bearer ${sessionStorage.getItem("token")}` );
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Show info for nav
-//    showInfoClient();
-//    showInfoBranch();
+document.addEventListener('DOMContentLoaded', async function() {
+	
+	if(sessionStorage.getItem("citaId")) {
+		const {id_service, appointment_date, appointmnet_time} = await obtenerCita();
+		cita.fecha = appointment_date;
+		cita.hora = `${appointmnet_time.split(":")[1]}:${appointmnet_time.split(":")[2]}`
+		cita.servicios = objToArrayId(id_service);
+	}
     
     // Show services
     showServices();
@@ -42,36 +46,6 @@ document.addEventListener('DOMContentLoaded', function() {
     //mostrarResumen();
 });
 
-function removeService(id) {
-    const { servicios } = cita;
-    cita.servicios = servicios.filter(servicio => servicio.id !== id);
-}
-
-function addService(servicioObj) {
-    const { servicios } = cita;
-    cita.servicios = [...servicios, servicioObj];
-}
-
-function nextPage() {
-    const nextPage = document.querySelector(`#siguiente`);
-    if(nextPage != null){
-        nextPage.addEventListener("click", () =>{
-            page++;
-            pagerButtons();
-        })
-    }
-}
-
-function previousPage() {
-    const previousPage = document.querySelector(`#anterior`);
-    if(previousPage != null){
-        previousPage.addEventListener("click", () =>{
-            page--;
-            pagerButtons();
-        })
-    }
-}
-
 function pagerButtons(){
     const nextPage = document.querySelector("#siguiente");
     const previosPage = document.querySelector("#anterior");
@@ -88,65 +62,9 @@ function pagerButtons(){
     mostrarSeccion(); // Cambia la sección que se muestra por la de la página
 }
 
-function mostrarSeccion() {
-    // Eliminar mostrar-seccion de la sección anterior
-    const seccionAnterior = document.querySelector('.mostrar-seccion');
-    if (seccionAnterior) {
-        seccionAnterior.classList.remove('mostrar-seccion');
-    }
-    const seccionActual = document.querySelector(`#paso-${page}`);
-    seccionActual.classList.add('mostrar-seccion');
-}
-
-function backDateDisabled() {
-    const inputFecha = document.querySelector('#fecha');
-
-    const fechaAhora = new Date();
-    const year = fechaAhora.getFullYear();
-    const mes = fechaAhora.getMonth() + 1;
-    const dia = fechaAhora.getDate() + 1;
-    const fechaDeshabilitar = `${year}-${mes}-${dia}`;
-
-    inputFecha.min = fechaDeshabilitar;
-}
-
-async function horaCita() {
-    const resultadoBranch = await fetch(`/api/branch/${branchId}`,{method: 'GET', headers: myHeaders, redirect: 'follow'});
-    const branch = await resultadoBranch.json();
-    const inputHora = document.querySelector('#hora');
-    inputHora.addEventListener('input', e => {
-        const horaCita = e.target.value;
-        const hora = horaCita.split(':');
-        const { scheduleOpen, scheduleClose } = branch;
-        if(hora[0] >= scheduleOpen.split(":")[0] && hora[0] <= scheduleClose.split(":")[0]) {
-            cita.hora = horaCita;
-        } else {
-            console.error("Es muy temprano");
-            setTimeout(() => {
-                inputHora.value = '';
-            }, 3000);
-        }
-    });
-}
-
-function fechaCita() {
-    const fechaInput = document.querySelector('#fecha');
-    fechaInput.addEventListener('input', e => {
-        const dia = new Date(e.target.value).getUTCDay();
-        if ([0, 6].includes(dia)) {
-            e.preventDefault();
-            fechaInput.value = '';
-            mostrarAlerta('Fines de Semana no son permitidos', 'error');
-        } else {
-            cita.fecha = fechaInput.value;
-        }
-    })
-}
-
 async function mostrarResumen() {
     cita.userId = userId;
     cita.branchId = branchId;
-
 
     // Destructuring
     const { servicios, fecha, hora } = cita;
@@ -166,20 +84,11 @@ async function mostrarResumen() {
 
         // agregar a resumen Div
         resumenDiv.appendChild(noServicios);
-        /*Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Faltan datos de Servicios, hora, fecha!',
-        })*/
         return;
     }
 
     const headingCita = document.createElement('H1');
     headingCita.textContent = 'Resumen de cita';
-
-    const resultadoUsuario = await fetch(`/api/user/${userId}`,{method: 'GET', headers: myHeaders, redirect: 'follow'});
-    const client = await resultadoUsuario.json();
-    const { name, lastName } = client;
 
     // Mostrar el resumen
     const fechaCita = document.createElement('P');
@@ -239,8 +148,8 @@ async function mostrarResumen() {
     btnReserve.classList.add("btn-principal");
     btnReserve.classList.add("btn-reservar");
     btnReserve.textContent = "Reservar cita";
-    btnReserve.onclick = makeAnAppointment;
-
+    (sessionStorage.getItem("citaId")) ? btnReserve.textContent = "Actualizar Cita" : "Reservar Cita";
+    (sessionStorage.getItem("citaId")) ? btnReserve.onclick = updateAppointment : makeAnAppointment;  
     resumenDiv.appendChild(btnReserve);
 }
 
@@ -251,7 +160,7 @@ async function makeAnAppointment(){
         servicesId.push(service.id)
     })
     myHeaders.append("Content-Type", "application/json");
-    const resultadoCita = await fetch(`/api/appointment/guardar`,{
+    const resultadoCita = await fetch(`${BASE_URL}api/appointment/guardar`,{
         method: 'POST',
         headers: myHeaders,
         body: JSON.stringify({
@@ -265,6 +174,31 @@ async function makeAnAppointment(){
     });
     const respuesta = await resultadoCita.json();
     if(respuesta){
-		enviarMultimediaCorreo(userId, branchId, respuesta.id)
+		enviarMultimediaCorreo(userId, branchId, respuesta.id, true)
+    }
+}
+
+async function updateAppointment(){
+    const { branchId, fecha, hora, servicios, userId} = cita;
+    const servicesId = [];
+    servicios.forEach(service => {
+        servicesId.push(service.id)
+    })
+    myHeaders.append("Content-Type", "application/json");
+    const resultadoCita = await fetch(`${BASE_URL}api/appointment/actualizar/${sessionStorage.getItem("citaId")}`,{
+        method: 'PUT',
+        headers: myHeaders,
+        body: JSON.stringify({
+            "id_branch": parseInt(branchId),
+            "appointment_date": fecha,
+            "appointment_time": `00:${hora}`,
+            "id_service": servicesId,
+            "id_client": parseInt(userId)
+        }),
+        redirect: 'follow'
+    });
+    const respuesta = await resultadoCita.json();
+    if(respuesta){
+		enviarMultimediaCorreo(userId, branchId, respuesta.id, false)
     }
 }
