cfea496e34d2a8ea777a62e517276aa8ac02a28b
Luis Gerardo Valencia Camacho <gevalencia99@gmail.com>
2023-05-01 10:40:06 -0600

    Ver servicios en resumen

--- src/main/java/com/esthetic/reservations/api/repository/AppointmentRepository.java
+++ src/main/java/com/esthetic/reservations/api/repository/AppointmentRepository.java
@@ -1,9 +1,12 @@
 package com.esthetic.reservations.api.repository;
 
 import java.util.ArrayList;
+import java.sql.Date;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import com.esthetic.reservations.api.model.Appointment;
@@ -14,6 +17,9 @@ import com.esthetic.reservations.api.model.UserEntity;
 @Component("AppointmentRepository")
 public interface AppointmentRepository extends GenericRepository<Appointment, Long> {
 	
+	@Query(value = "select * from appointment where appointment_date = :date and id_branch_id = :id", nativeQuery = true)
+	ArrayList<Appointment> findByDate(@Param("date") String date,@Param("id") String id);
+		
 	ArrayList<Appointment> findAllByIdBranch(Branch idbranch);
 	
 	ArrayList<Appointment> findAllByIdClient(UserEntity idClient);
