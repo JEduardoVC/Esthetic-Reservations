839e43af87ba7ea9d6da3beb98795b126a0de3f4
ezequielgame <ezequiel.gmejia@gmail.com>
2023-05-10 21:12:21 -0600

    Comment no used code

--- src/main/resources/static/js/manager.js
+++ src/main/resources/static/js/manager.js
@@ -122,42 +122,42 @@ async function getBranchEmployees() {
     return html;
 }
 
-// Promedio de puntuacion de acuerdo a su tabla
-const score = 4;
-const stars = document.querySelectorAll("#star_id");
-stars.forEach(function () {
-    for (let i = 0; i < score; i++) {
-        stars[i].classList.add("checked");
-    }
-});
+// // Promedio de puntuacion de acuerdo a su tabla
+// const score = 4;
+// const stars = document.querySelectorAll("#star_id");
+// stars.forEach(function () {
+//     for (let i = 0; i < score; i++) {
+//         stars[i].classList.add("checked");
+//     }
+// });
 
-//Ultima puntuacion dada a este empleado
-const score_comment = 2;
-const stars_comment = document.querySelectorAll("#star-comment_id");
-stars_comment.forEach(function () {
-    for (let i = 0; i < score_comment; i++) {
-        stars_comment[i].classList.add("checked");
-    }
-});
-
-
-////////// Esta parte es igual a la de arriba, solo es para saber que se cambia el id por el ID que viene de la base   /////////
+// //Ultima puntuacion dada a este empleado
+// const score_comment = 2;
+// const stars_comment = document.querySelectorAll("#star-comment_id");
+// stars_comment.forEach(function () {
+//     for (let i = 0; i < score_comment; i++) {
+//         stars_comment[i].classList.add("checked");
+//     }
+// });
 
 
-// Promedio de puntuacion de acuerdo a su tabla
-const score2 = 3;
-const stars2 = document.querySelectorAll("#star_id2");
-stars2.forEach(function () {
-    for (let i = 0; i < score2; i++) {
-        stars2[i].classList.add("checked");
-    }
-});
+// ////////// Esta parte es igual a la de arriba, solo es para saber que se cambia el id por el ID que viene de la base   /////////
 
-//Ultima puntuacion dada a este empleado
-const score_comment2 = 5;
-const stars_comment2 = document.querySelectorAll("#star-comment_id2");
-stars_comment2.forEach(function () {
-    for (let i = 0; i < score_comment2; i++) {
-        stars_comment2[i].classList.add("checked");
-    }
-});
+
+// // Promedio de puntuacion de acuerdo a su tabla
+// const score2 = 3;
+// const stars2 = document.querySelectorAll("#star_id2");
+// stars2.forEach(function () {
+//     for (let i = 0; i < score2; i++) {
+//         stars2[i].classList.add("checked");
+//     }
+// });
+
+// //Ultima puntuacion dada a este empleado
+// const score_comment2 = 5;
+// const stars_comment2 = document.querySelectorAll("#star-comment_id2");
+// stars_comment2.forEach(function () {
+//     for (let i = 0; i < score_comment2; i++) {
+//         stars_comment2[i].classList.add("checked");
+//     }
+// });
